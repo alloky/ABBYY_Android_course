b4812eeec71427faf40895f698351261b98f0ad4
Alloky Alloky <alloky2011@gmail.com>
2019-12-07 13:54:51 +0300

    task_5

---
+++ task_5/IStorage.kt
@@ -0,0 +1,17 @@
+package task_5
+
+interface IStorage {
+
+    fun put(key: String, value: String)
+
+    fun put(key: String, value: Int)
+
+    fun put(key: String, value: Long)
+
+    fun put(key: String, value: Char)
+
+    fun put(key: String, value: Double)
+
+    fun get(key: String): Any?
+
+}
